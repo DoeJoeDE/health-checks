792e219b2d87ca6542d0ef38b46b32142ecfd3d0
Florian Caro <floriancaro0510@gmail.com>
2021-03-30 11:32:57 +0200

    Add example code

--- disk_usage.py
+++ disk_usage.py
@@ -3,4 +3,8 @@
 def main():
     pass
 
+def check_root_full():
+    """Returns True if the root partition is full, False otherwise."""
+    return check_disk_full(disk="/",min_gb=2, min_percent=2)
+
 main()
