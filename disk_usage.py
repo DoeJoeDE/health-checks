7e44021e7925320afe9b24e6660f004af59cb272
Florian Caro <floriancaro0510@gmail.com>
2021-03-30 11:34:59 +0200

    Add even more example code

--- disk_usage.py
+++ disk_usage.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python3
+import sys
 
 def main():
-    pass
+    checks=[
+        (check_reboot, "Pending Reboot"),
+        (check_root_full, "Root partition full"),
+    ]
+    for check, msg in checks:
+        if check():
+            print(msg)
+            sys.exit(1)
 
 def check_root_full():
     """Returns True if the root partition is full, False otherwise."""
